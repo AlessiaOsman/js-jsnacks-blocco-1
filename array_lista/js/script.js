a680c439609dd6d6e25026d0f3cb2ef24afa1bd0
AlessiaOsman <alessia.osman@gmail.com>
2023-12-22 18:58:24 +0100

    ciclo for, assegnazione messaggio e stampa in console

--- array_lista/js/script.js
+++ array_lista/js/script.js
@@ -12,7 +12,7 @@ const basketFruit = [
     'fragola'
 ]
 
-console.log(basketFruit)
+
 
 /**Aggiungo la pesca alla lista */
 
@@ -21,3 +21,30 @@ basketFruit.push('pesca')
 console.log(basketFruit)
 
 let message = ''
+
+const cocktailFruit = 'cocomero';
+
+// crreo Flag
+
+let isThere = false
+
+// Creo ciclo for per controllare se ho l'ingrediente nella lista
+
+for (let i = 0; i<= basketFruit.length && !isThere; i++){
+    if (cocktailFruit === basketFruit[i]){
+        isThere = true
+    }
+}
+
+//Assegno il testo al messaggio
+
+if (isThere){
+    message += 'Trovato! Devo solo preparare il cocktail.'
+} else {
+    message += 'Oh no, devo uscire a comprare il cocomero!'
+}
+
+//Stampo messaggio in console
+
+console.log(message)
+
