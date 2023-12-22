d420eb33f6a2d2a5c597f3776a8ce3cbb10ca6f7
AlessiaOsman <alessia.osman@gmail.com>
2023-12-22 17:55:39 +0100

    aggiunta lista

---
+++ array_lista/js/script.js
@@ -0,0 +1,15 @@
+/*Creo lista frutta*/
+
+const basketFruit = [
+    'banana', 
+    'mela', 
+    'pera', 
+    'ciliegia', 
+    'arancia', 
+    'mandarino', 
+    'cocomero', 
+    'limone', 
+    'fragola'
+]
+
+console.log(basketFruit)
